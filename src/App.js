7036511c7fbef2a325a3ebe82c4f1a08f3803641
nurealammiaji <nurealam.miaji@gmail.com>
2023-07-11 10:46:09 +0600

    new components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Products from './components/Products/Products';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Products></Products>
     </div>
   );
 }
---
+++ src/components/Products/Products.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Products = () => {
+
+    return (
+        <div>
+            <h2>Hello from Products</h2>
+        </div>
+    );
+};
+
+export default Products;
