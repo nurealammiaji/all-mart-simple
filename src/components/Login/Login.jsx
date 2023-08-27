49a507d95ee0b8a2f38b23c304f5851aa6037aba
nurealammiaji <nurealam.miaji@gmail.com>
2023-08-27 12:50:28 +0600

    login page modified

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,27 @@
+import "./Login.css";
+
 const Login = () => {
     return (
         <div>
-            <h3>Login Page</h3>
+            <br /><br />
+            <div className="form-container">
+                <h2>Login</h2>
+                <form>
+                    <div className="form-control">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" required />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" required />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="password">Confirm Password</label>
+                        <input type="password" name="confirm-password" required />
+                    </div>
+                    <button type="submit">Login</button>
+                </form>
+            </div>
         </div>
     );
 };
